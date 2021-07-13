208a9df13bca4ccb0f4c90482760b8ada502fdc0
FedericoAcostaDev <fedeacos045@gmail.com>
2021-07-13 18:31:10 -0300

    profile added

--- server.js
+++ server.js
@@ -1,64 +1,76 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json()) // To parse the incoming requests with JSON payloads
-
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // To parse the incoming requests with JSON payloads
 
 //without moddifications
 
 const database = {
-    users: [
-        {
-            id: '1',
-            name:'john',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '2',
-            name:'sally',
-            email: 'sally@gmail.com',
-            password: 'bananas',
-            entries: 0,
-            joined: new Date()
-        }
-    ]
-}
+  users: [
+    {
+      id: "1",
+      name: "john",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "2",
+      name: "sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
 
-app.get('/', (req, res) => {
-    res.send(database.users);
-})
+app.get("/", (req, res) => {
+  res.send(database.users);
+});
 
-app.post ('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email &&
-    req.body.password ===  database.users[0].password) {  
-    res.json('success');
-} else {
-    res.status(400).json('error logging in');
-}
-})
+app.post("/signin", (req, res) => {
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
+});
 
-app.post('/register', (req, res) => {
-    const { email, name, password} = req.body;
-    database.users.push({
-            id: '1',
-            name:name,
-            email: email,
-            password: password,
-            entries: 0,
-            joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
-})
- 
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "1",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+app.get("/profile/:id", (req, res) => {
+  const { id } = req.params;
+  let found = false;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      found = true;
+      return res.json(user);
+    }
+  })
+  if (!found) {
+      res.status(400).json(' user not found');
+  }
+});
 app.listen(3000, () => {
-    console.log('app is running on port 3000');
-})
+  console.log("app is running on port 3000");
+});
 
 /*
 /--> res= this is working
