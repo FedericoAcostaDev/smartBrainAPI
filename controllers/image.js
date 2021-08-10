f7321afb51dc64237eff9b4559503cd7ef916890
FedericoAcostaDev <fedeacos045@gmail.com>
2021-08-10 17:39:22 -0300

    nav buttons fixed

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,19 @@
-handleImage = (req, res, db) => {
+const Clarifai = require ("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "1a0efca46056437bbbeeb505fe64c696",
+});
+
+const handleApiCall = (req, res) => {
+app.models
+.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+.then(data => {
+  res.json(data);
+})
+.catch( err => res.status(400).json('unable to work whit API'))
+}
+
+const handleImage = (req, res, db) => {
     const { id } = req.body;
     db("users")
       .where("id", "=", id)
@@ -11,5 +26,6 @@ handleImage = (req, res, db) => {
   };
 
   module.exports = {
-      handleImage
+      handleImage,
+      handleApiCall
   }
--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.post("/signin", signin.handleSignin(db, bcrypt)) //trying a cleaner look
 app.post("/register", (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get("/profile/:id", (req, res)=> {profile.handleProfileGet(req, res, db)})
 app.put("/image", (req, res) => { image.handleImage(req, res, db)})
+app.post("/imageurl", (req, res) => { image.handleApiCall(req, res,)})
 
 /*/ Load hash from your password DB.
 bcrypt.compare("bacon", hash, function(err, res) {
